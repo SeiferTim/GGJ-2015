358ad641cb5a984706e26cd4cf6034e1cbd00c9f
Tim <seifertim@gmail.com>
2015-01-23 23:26:33 -0600

    no message

---
+++ source/Game.hx
@@ -0,0 +1,14 @@
+package ;
+
+class Game
+{
+
+	public var positions:Array<Int>;	// random planet locations
+	public var players:Int;				// number of planets in the game
+	public var playerTurn:Int; 			// who's turn it is now
+	public var turn:Int;				// current turn
+	public var maxTurns:Int;			// number of turns until game ends
+	
+	
+	
+}
